32c8acfdf40c0c1dd6596502025844e6fab177d2
Ron-Sungcang <sungcangjoseph@gmail.com>
2023-12-23 02:58:38 -0600

    made a new starting strength screen branch, started startingstrength screen, and made excercise model

---
+++ app/src/main/java/com/example/a30daysfitness/StartingStrengthScreen.kt
@@ -0,0 +1,4 @@
+package com.example.a30daysfitness
+
+class StartingStrengthScreen {
+}
---
+++ app/src/main/java/com/example/a30daysfitness/model/ExcerciseRepository.kt
@@ -0,0 +1,15 @@
+package com.example.a30daysfitness.model
+
+import com.example.a30daysfitness.R
+
+object ExcerciseRepository {
+    val starting1a = listOf(
+        Excercises(
+            nameRes = R.string.squat,
+            descriptionRes = R.string.squatdesc,
+            imageRes = R.drawable._2560px_squats_svg,
+            reps = 5,
+            sets = 3
+        ),
+    )
+}
