eecbf5b8f4294fcc97c1ad7b4c71ef56a9e47387
Ron-Sungcang <sungcangjoseph@gmail.com>
2023-12-24 00:11:20 -0600

    finished up the excercises repository for starting screen, added a few more strings

--- app/src/main/java/com/example/a30daysfitness/model/ExcerciseRepository.kt
+++ app/src/main/java/com/example/a30daysfitness/model/ExcerciseRepository.kt
@@ -11,5 +11,137 @@ object ExcerciseRepository {
             reps = 5,
             sets = 3
         ),
+        Excercises(
+            nameRes = R.string.bench,
+            descriptionRes = R.string.benchdesc,
+            imageRes = R.drawable.bench_press_1_svg,
+            reps = 5,
+            sets = 3
+        ),
+        Excercises(
+            nameRes = R.string.deadlift,
+            descriptionRes = R.string.deadliftdesc,
+            imageRes = R.drawable._1200px_rear_deltoid_row_barbell_1_svg,
+            reps = 5,
+            sets = 1
+        )
+    )
+
+    val starting1b = listOf(
+        Excercises(
+            nameRes = R.string.squat,
+            descriptionRes = R.string.squatdesc,
+            imageRes = R.drawable._2560px_squats_svg,
+            reps = 5,
+            sets = 3
+        ),
+        Excercises(
+            nameRes = R.string.shoulderp,
+            descriptionRes = R.string.shoulderpdesc,
+            imageRes = R.drawable._4580253_200,
+            reps = 5,
+            sets = 3
+        ),
+        Excercises(
+            nameRes = R.string.deadlift,
+            descriptionRes = R.string.deadliftdesc,
+            imageRes = R.drawable._1200px_rear_deltoid_row_barbell_1_svg,
+            reps = 5,
+            sets = 1
+        )
+    )
+
+    val starting2a = listOf(
+        Excercises(
+            nameRes = R.string.squat,
+            descriptionRes = R.string.squatdesc,
+            imageRes = R.drawable._2560px_squats_svg,
+            reps = 5,
+            sets = 3
+        ),
+        Excercises(
+            nameRes = R.string.bench,
+            descriptionRes = R.string.benchdesc,
+            imageRes = R.drawable.bench_press_1_svg,
+            reps = 5,
+            sets = 3
+        ),
+        Excercises(
+            nameRes = R.string.deadlift,
+            descriptionRes = R.string.deadliftdesc,
+            imageRes = R.drawable._1200px_rear_deltoid_row_barbell_1_svg,
+            reps = 5,
+            sets = 1
+        )
+    )
+
+    val starting2b = listOf(
+        Excercises(
+            nameRes = R.string.squat,
+            descriptionRes = R.string.squatdesc,
+            imageRes = R.drawable._2560px_squats_svg,
+            reps = 5,
+            sets = 3
+        ),
+        Excercises(
+            nameRes = R.string.shoulderp,
+            descriptionRes = R.string.shoulderpdesc,
+            imageRes = R.drawable._4580253_200,
+            reps = 5,
+            sets = 3
+        ),
+        Excercises(
+            nameRes = R.string.clean,
+            descriptionRes = R.string.cleandesc,
+            imageRes = R.drawable._2227543,
+            reps = 3,
+            sets = 5
+        )
+    )
+    val starting3a = listOf(
+        Excercises(
+            nameRes = R.string.squat,
+            descriptionRes = R.string.squatdesc,
+            imageRes = R.drawable._2560px_squats_svg,
+            reps = 5,
+            sets = 3
+        ),
+        Excercises(
+            nameRes = R.string.bench,
+            descriptionRes = R.string.benchdesc,
+            imageRes = R.drawable.bench_press_1_svg,
+            reps = 5,
+            sets = 3
+        ),
+        Excercises(
+            nameRes = R.string.deadclean,
+            descriptionRes = R.string.deadcleandesc,
+            imageRes = R.drawable._2227543,
+            reps = 5,
+            sets= 1
+        )
+    )
+    val starting3b = listOf(
+        Excercises(
+            nameRes = R.string.squat,
+            descriptionRes = R.string.squatdesc,
+            imageRes = R.drawable._2560px_squats_svg,
+            reps = 5,
+            sets = 3
+        ),
+        Excercises(
+            nameRes = R.string.shoulderp,
+            descriptionRes = R.string.shoulderpdesc,
+            imageRes = R.drawable._4580253_200,
+            reps = 5,
+            sets = 3
+        ),
+        Excercises(
+            nameRes = R.string.chinup,
+            descriptionRes = R.string.chinupdesc,
+            imageRes = R.drawable.download,
+            reps = 0,
+            sets = 0
+        )
     )
 }
