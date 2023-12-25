cd26db1c894d41a37290270793be1c5de1fa1d49
Ron-Sungcang <sungcangjoseph@gmail.com>
2023-12-24 18:14:11 -0600

    added composable function StartingStrengthList to display each list of excercises. Added composable function StartingStrengthLazyList to display everything in a scrollable fashion

--- app/src/main/java/com/example/a30daysfitness/StartingStrengthScreen.kt
+++ app/src/main/java/com/example/a30daysfitness/StartingStrengthScreen.kt
@@ -5,6 +5,7 @@ import androidx.annotation.StringRes
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
@@ -12,6 +13,8 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.sizeIn
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.MaterialTheme
@@ -24,10 +27,99 @@ import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import com.example.a30daysfitness.model.ExcerciseRepository
 import com.example.a30daysfitness.model.Excercises
 import com.example.a30daysfitness.ui.theme._30DaysFitnessTheme
 
 class StartingStrengthScreen {
+
+    /**
+     * A composable function that consists of a lazycolumn to display
+     * everything in a scrollable fashion
+     */
+    @Composable
+    fun StartingStrengthLazyList(
+        excerciselist1a: List<Excercises>,
+        excerciselist1b: List<Excercises>,
+        excerciselist2a: List<Excercises>,
+        excerciselist2b: List<Excercises>,
+        excerciselist3a: List<Excercises>,
+        excerciselist3b: List<Excercises>,
+        modifier: Modifier = Modifier,
+        contentPadding: PaddingValues = PaddingValues(0.dp)
+    ) {
+        LazyColumn(modifier = modifier) {
+            item {
+                Text(
+                    text = stringResource(id = R.string.programdesc1),
+                    style = MaterialTheme.typography.bodyLarge
+                )
+                Text(
+                    text = stringResource(id = R.string.phase1),
+                    style = MaterialTheme.typography.displayLarge
+                )
+            }
+            item {
+                StartingStrengthList(excerciselist = excerciselist1a, R.string.dayA)
+            }
+            item {
+                StartingStrengthList(excerciselist = excerciselist1b, R.string.dayB)
+            }
+            item {
+                Text(
+                    text = stringResource(id = R.string.phase2),
+                    style = MaterialTheme.typography.displayLarge
+                )
+            }
+            item {
+                StartingStrengthList(excerciselist = excerciselist2a, R.string.dayA)
+            }
+            item {
+                StartingStrengthList(excerciselist = excerciselist2b, R.string.dayB)
+            }
+            item {
+                Text(
+                    text = stringResource(id = R.string.phase3),
+                    style = MaterialTheme.typography.displayLarge
+                )
+            }
+            item {
+                StartingStrengthList(excerciselist = excerciselist3a, R.string.dayA)
+            }
+            item {
+                StartingStrengthList(excerciselist = excerciselist3b, R.string.dayB)
+            }
+        }
+    }
+
+    /**
+     * A composable function that is for displaying individual list pf excercises
+     */
+    @Composable
+    fun StartingStrengthList(
+        excerciselist: List<Excercises>,
+        @StringRes dayRes: Int,
+        modifier: Modifier = Modifier
+    ){
+        Column(modifier = modifier) {
+            Text(
+                text = stringResource(id = R.string.dayA),
+                style = MaterialTheme.typography.displaySmall,
+                modifier = Modifier.padding(start = 8.dp)
+            )
+            Column{
+                excerciselist.forEach { excercises ->
+                    FitItem(
+                        excercise = excercises,
+                        modifier = modifier
+                            .padding(8.dp)
+                    )
+                }
+            }
+        }
+    }
+
+
 
     /**
      * A composable function that is responsible for cards that display
@@ -94,6 +186,14 @@ class StartingStrengthScreen {
     @Composable
     private fun FitPreview(){
         _30DaysFitnessTheme {
+            StartingStrengthLazyList(
+                excerciselist1a = ExcerciseRepository.starting1a,
+                excerciselist1b = ExcerciseRepository.starting1b,
+                excerciselist2a = ExcerciseRepository.starting2a,
+                excerciselist2b = ExcerciseRepository.starting2b,
+                excerciselist3a = ExcerciseRepository.starting3a,
+                excerciselist3b = ExcerciseRepository.starting3b
+            )
         }
     }
 }
