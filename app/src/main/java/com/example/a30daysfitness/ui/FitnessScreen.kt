292d2b9fd8ba2841dc0d8c2356aa65a02d3c2a8d
Ron-Sungcang <sungcangjoseph@gmail.com>
2024-01-13 00:34:43 -0600

    Created Fit Lazy List to display the programs in the homescreen

--- app/src/main/java/com/example/a30daysfitness/ui/FitnessScreen.kt
+++ app/src/main/java/com/example/a30daysfitness/ui/FitnessScreen.kt
@@ -8,6 +8,8 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.MaterialTheme
@@ -15,17 +17,31 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.a30daysfitness.R
+import com.example.a30daysfitness.model.ExcerciseRepository
 import com.example.a30daysfitness.model.Programs
 import com.example.a30daysfitness.ui.theme._30DaysFitnessTheme
-
+import javax.sql.DataSource
 
 @Composable
-fun FitItem(modifier: Modifier = Modifier){
+fun FitLazyList(programList: List<Programs>,modifier: Modifier = Modifier){
+    LazyColumn(modifier = modifier){
+        items(programList){programs ->
+            FitItem(
+                programs = programs,
+                modifier = Modifier.padding(8.dp)
+                )
+        }
+    }
+}
+
+@Composable
+fun FitItem(programs: Programs, modifier: Modifier = Modifier){
     Card(
         modifier = modifier,
         shape = MaterialTheme.shapes.medium,
@@ -36,13 +52,13 @@ fun FitItem(modifier: Modifier = Modifier){
             .padding(16.dp)
         ) {
             Text(
-                text = stringResource(id = R.string.program1),
+                text = LocalContext.current.getString(programs.nameRes),
                 style = MaterialTheme.typography.displayLarge
                 )
             Spacer(Modifier.width(16.dp))
             Box{
-                Image(painter = painterResource(id = R.drawable.starting_strength),
-                    contentDescription = stringResource(id = R.string.program1),
+                Image(painter = painterResource(programs.imageRes),
+                    contentDescription = LocalContext.current.getString(programs.nameRes),
                     modifier = Modifier
                         .fillMaxWidth()
                         .height(194.dp),
@@ -51,7 +67,7 @@ fun FitItem(modifier: Modifier = Modifier){
             }
             Spacer(Modifier.height(16.dp))
             Text(
-                text = stringResource(id = R.string.program1desc),
+                text = LocalContext.current.getString(programs.descriptionRes),
                 style = MaterialTheme.typography.bodyLarge
                 )
         }
@@ -62,6 +78,6 @@ fun FitItem(modifier: Modifier = Modifier){
 @Composable
 private fun FitPreview(){
     _30DaysFitnessTheme {
-        FitItem()
+        FitLazyList(programList = ExcerciseRepository.programs)
     }
 }
