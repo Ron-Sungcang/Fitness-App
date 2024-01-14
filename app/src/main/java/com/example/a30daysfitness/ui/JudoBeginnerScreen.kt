f1d804caba09fa55aaadf435454bbc114181ad63
Ron-Sungcang <sungcangjoseph@gmail.com>
2024-01-13 19:01:21 -0600

    Added a bunch of drawables and strings, added excercises for the judo beginner screen

---
+++ app/src/main/java/com/example/a30daysfitness/ui/JudoBeginnerScreen.kt
@@ -0,0 +1,16 @@
+package com.example.a30daysfitness.ui
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.a30daysfitness.ui.theme._30DaysFitnessTheme
+
+@Composable
+fun JudoItem(){}
+
+@Preview
+@Composable
+private fun JudoBeginnerScreen(){
+    _30DaysFitnessTheme {
+
+    }
+}
